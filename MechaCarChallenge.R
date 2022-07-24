7311dc99dba88011358c552cfc68a7e7292136fd
Steve Zimmermann <zimmer3.iii@gmail.com>
2022-07-24 15:12:22 -0700

    Finished Trip Analysis R script.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,9 +4,22 @@
 
 #Import
 library(dplyr)
+library(tidyverse)
 
 #Read CSV
 MechaCar_mpg <- read.csv('MechaCar_mpg.csv')
 
 #Linear Regression
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg))
+
+
+#Visualizations for Trip Analysis
+
+#Read CSV
+Coil_Table <- read.csv('Suspension_Coil.csv')
+
+#Summary Table
+total_summary <- Coil_Table %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Variance_PSI=var(PSI),SD_PSI=sd(PSI), .groups ='keep')
+
+#Lot Summary
+lot_summary <- Coil_Table %>% group_by(Manufacturing_Lot)  %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Variance_PSI=var(PSI),SD_PSI=sd(PSI), .groups ='keep')
